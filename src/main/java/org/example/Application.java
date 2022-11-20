3f9462fb661e63011a4f5eee34314a7fb344a9a6
joaooliveiraa11 <93929858+joaooliveiraa11@users.noreply.github.com>
2022-11-20 16:29:54 +0000

    Classes rebuild

--- src/main/java/org/example/Application.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-import java.io.IOException;
-
-public class Application {
-    public static void main(String[] args) throws IOException {
-        Game game = new Game();
-    }
-}
--- src/main/java/org/example/Menu/InstructionsMenu.java
+++
@@ -1,4 +0,0 @@
-package org.example.Menu;
-
-public class InstructionsMenu {
-}
