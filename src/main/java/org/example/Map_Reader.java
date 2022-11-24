646accdadf8d392c882b97bb16b3e63ccaeedb12
joaooliveiraa11 <93929858+joaooliveiraa11@users.noreply.github.com>
2022-11-24 15:04:17 +0000

    Reader method complete

--- src/main/java/org/example/Map_Reader.java
+++ src/main/java/org/example/Map_Reader.java
@@ -1,16 +1,22 @@
 package org.example;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Map_Reader {
     private File file;
     private Scanner scanner;
 
-    public void MAP1_READER() throws FileNotFoundException {
+    public ArrayList<String> MAP1_READER() throws FileNotFoundException {
+        ArrayList<String> map1lines = new ArrayList<>();
         file = new File("level1.txt");
         scanner = new Scanner(file);
         String line = "";
-        //while(scanner.hasNextLine()){}
+        while(scanner.hasNextLine()){
+            line = scanner.nextLine();
+            map1lines.add(line);
+        }
+        return map1lines;
     }
 }
