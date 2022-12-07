b699331959a4600e9ca209dcdb3929c2772ef6dd
joaooliveiraa11 <93929858+joaooliveiraa11@users.noreply.github.com>
2022-12-07 00:46:34 +0000

    More tests

--- src/test/java/com/aor/hero/gui/LanternaGUITest.java
+++ src/test/java/com/aor/hero/gui/LanternaGUITest.java
@@ -27,9 +27,41 @@ class LanternaGUITest {
     @Test
     void drawHero() {
         gui.drawPacman(new Position(1, 1));
-
+        //FFD700
         Mockito.verify(tg, Mockito.times(1)).setForegroundColor(new TextColor.RGB(255, 215, 0));
         Mockito.verify(tg, Mockito.times(1)).putString(1, 2, "H");
+    }
+
+    @Test
+    void drawWall() {
+        gui.drawWall(new Position(1, 1));
+
+        Mockito.verify(tg, Mockito.times(1)).setForegroundColor(new TextColor.RGB(51, 51, 255));
+        Mockito.verify(tg, Mockito.times(1)).putString(1, 2, "P");
+    }
+
+    @Test
+    void drawMonster() {
+        gui.drawMonster(new Position(1, 1));
+
+        Mockito.verify(tg, Mockito.times(1)).setForegroundColor(new TextColor.RGB(204, 0, 0));
+        Mockito.verify(tg, Mockito.times(1)).putString(1, 2, "@");
+    }
+
+    @Test
+    void drawCoin() {
+        gui.drawCoin(new Position(1, 1));
+
+        Mockito.verify(tg, Mockito.times(1)).setForegroundColor(new TextColor.RGB(218, 165, 32));
+        Mockito.verify(tg, Mockito.times(1)).putString(1, 2, "o");
+    }
+
+    @Test
+    void drawSuperCoin() {
+        gui.drawSupCoin(new Position(1, 1));
+
+        Mockito.verify(tg, Mockito.times(1)).setForegroundColor(new TextColor.RGB(218, 165, 32));
+        Mockito.verify(tg, Mockito.times(1)).putString(1, 2, "O");
     }
 
     @Test
